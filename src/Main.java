be612e7d7655f4941f80c1cf777cd52fbcb9af49
Olga <helga_ser@mail.ru>
2023-07-29 11:49:37 +0300

    Rename variable

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int ostshet = 100; // остаток на счете клиента
-        int popolnenie = 1001; // сумма пополнения
+        int balance = 100; // остаток на счете клиента
+        int amount = 1001; // сумма пополнения
         int bonus = 0; // начальное значение бонусов
 
-        if (popolnenie > 1000) {
-            bonus = popolnenie /100;
+        if (amount > 1000) {
+            bonus = amount /100;
         }
 
-        System.out.println("Остаток на счете после пополнения " +(ostshet + popolnenie + bonus));
+        System.out.println("Остаток на счете после пополнения " +(balance + amount + bonus));
         System.out.println("Количество бонусных рублей " + bonus);
 
 
